1d89566b8fded585c89a5fc857ee8b8eae12c7f8
michbalkany <askmitchb@gmail.com>
2022-12-06 15:49:13 +0000

    domain-generator

--- src/app.js
+++ src/app.js
@@ -6,6 +6,19 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let domainNames = [];
+  let ourlist = document.querySelector(".domains");
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        domainNames.push(pronoun[i] + adj[j] + noun[k] + ".com");
+      }
+    }
+  }
+  for (let i = 0; i < domainNames.length; i++) {
+    ourlist.innerHTML += "<li>" + domainNames[i] + "</li>";
+  }
 };
